0488fe7527301ec37e5b9e57c990aafcee27513a
Drake Emko <demko@revionics.com>
2013-12-19 20:03:03 -0700

    changed extends to CordovaActivity to avoid error "No Java files found which extend CordovaActivity"

--- platforms/android/src/net/edrake/adviceowl/AdviceOwl.java
+++ platforms/android/src/net/edrake/adviceowl/AdviceOwl.java
@@ -29,7 +29,7 @@ import com.google.ads.*;
 import com.google.analytics.tracking.android.EasyTracker;
 import com.google.analytics.tracking.android.Fields;
 
-public class AdviceOwl extends DroidGap {
+public class AdviceOwl extends CordovaActivity {
 	
     private String versionName = "0";
     private int versionCode = 0;
