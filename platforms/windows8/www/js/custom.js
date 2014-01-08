663a9c4b2324da8d005a91bff7199984c806105a
Drake Emko <drakee@gmail.com>
2014-01-08 01:34:11 -0700

    added cordova platform support for wp8 and windows8

---
+++ platforms/windows8/www/js/custom.js
@@ -0,0 +1,9 @@
+// this file, and each of its methods, should be overriden by an OS-specific custom.js from a merges/ directory
+
+function openLink(link) {
+	window.open(encodeURI(link)); 
+}
+
+function doCustomActions() {
+	// intentionally left blank
+}
