679342fc64095f125e53e5ce648875765f5ac9dd
Drake Emko <demko@revionics.com>
2014-01-16 08:43:55 -0700

    removed windows8 platform

--- platforms/windows8/www/js/custom.js
+++
@@ -1,9 +0,0 @@
-// this file, and each of its methods, should be overriden by an OS-specific custom.js from a merges/ directory
-
-function openLink(link) {
-	window.open(encodeURI(link)); 
-}
-
-function doCustomActions() {
-	// intentionally left blank
-}
