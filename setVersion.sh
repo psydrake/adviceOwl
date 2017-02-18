5eca8b5ed0e651bbd005d2b12601361682f8980d
Drake Emko <drakee@gmail.com>
2017-02-18 03:07:31 -0700

    incremented version. re-introduced compatible with android sdk 14

--- setVersion.sh
+++ setVersion.sh
@@ -8,7 +8,7 @@
 VERSION=1.8
 
 # Only used in Android. Appended as minor version number if avaliable
-VERSION_CODE=29
+VERSION_CODE=30
 
 perl -pi -e "s/\sversion=\"\d+\.\d+\"\s/\ version=\"${VERSION}\"\ /" www/config.xml
 perl -pi -e "s/\sandroid\-versionCode=\"\d+\"\s/\ android-versionCode=\"${VERSION_CODE}\"\ /" www/config.xml
@@ -20,3 +20,21 @@ perl -pi -e "s/\"version\"\:\ \"\d+\.\d+\.\d+\"/\"version\"\:\ \"${VERSION}\.${V
 perl -pi -e "s/\"version\"\:\ \"\d+\.\d+\.\d+\"/\"version\"\:\ \"${VERSION}\.${VERSION_CODE}\"/" webapp/merges/manifest.webapp
 perl -pi -e "s/\<appVersion\>\d+\.\d+\.\d+\<\/appVersion\>/\<appVersion\>${VERSION}\.${VERSION_CODE}\<\/appVersion\>/" platforms/wp8/analytics.xml
 perl -pi -e "s/\sVersion\=\"\d+\.\d+\"/ Version=\"${VERSION}\"/" platforms/wp8/Properties/WMAppManifest.xml
+
+perl -pi -e "s/\sversion=\"\d+\.\d+\"\s/\ version=\"${VERSION}\"\ /" platforms/android/assets/www/config.xml
+perl -pi -e "s/android\-versionCode=\"\d+\"/android\-versionCode=\"${VERSION_CODE}\"/" platforms/android/assets/www/config.xml
+perl -pi -e "s/\sversion=\"\d+\.\d+\"\s/\ version=\"${VERSION}\"\ /" platforms/android/res/xml/config.xml
+perl -pi -e "s/android\-versionCode=\"\d+\"/android\-versionCode=\"${VERSION_CODE}\"/" platforms/android/res/xml/config.xml
+perl -pi -e "s/\sversion=\"\d+\.\d+\"\s/\ version=\"${VERSION}\"\ /" platforms/android/build/intermediates/assets/debug/www/config.xml
+perl -pi -e "s/android\-versionCode=\"\d+\"/android\-versionCode=\"${VERSION_CODE}\"/" platforms/android/build/intermediates/assets/debug/www/config.xml
+perl -pi -e "s/\sversion=\"\d+\.\d+\"\s/\ version=\"${VERSION}\"\ /" platforms/android/build/intermediates/assets/release/www/config.xml
+perl -pi -e "s/android\-versionCode=\"\d+\"/android\-versionCode=\"${VERSION_CODE}\"/" platforms/android/build/intermediates/assets/release/www/config.xml
+perl -pi -e "s/\sversion=\"\d+\.\d+\"\s/\ version=\"${VERSION}\"\ /" platforms/android/build/intermediates/res/merged/debug/xml/config.xml
+perl -pi -e "s/android\-versionCode=\"\d+\"/android\-versionCode=\"${VERSION_CODE}\"/" platforms/android/build/intermediates/res/merged/debug/xml/config.xml
+perl -pi -e "s/\sversion=\"\d+\.\d+\"\s/\ version=\"${VERSION}\"\ /" platforms/android/build/intermediates/res/merged/release/xml/config.xml
+perl -pi -e "s/android\-versionCode=\"\d+\"/android\-versionCode=\"${VERSION_CODE}\"/" platforms/android/build/intermediates/res/merged/release/xml/config.xml
+perl -pi -e "s/\sversion=\"\d+\.\d+\"\s/\ version=\"${VERSION}\"\ /" webapp/www/config.xml
+perl -pi -e "s/android\-versionCode=\"\d+\"/android\-versionCode=\"${VERSION_CODE}\"/" webapp/www/config.xml
+
+perl -pi -e "s/android\:versionCode=\"\d+\"/android\:versionCode=\"${VERSION_CODE}\"/" platforms/android/build/intermediates/manifests/full/debug/AndroidManifest.xml
+perl -pi -e "s/android\:versionCode=\"\d+\"/android\:versionCode=\"${VERSION_CODE}\"/" platforms/android/build/intermediates/manifests/full/release/AndroidManifest.xml
